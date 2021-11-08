68967596f344e269e2667fec4251dd1bff2bd9b8
Rennan Franco Chagas <rennan.chagas@championx.com.br>
2021-11-08 00:55:33 -0600

    added jpa and devtools; added model profile and sector

---
+++ src/main/java/com/rennan/sectorspringboot/domain/service/Profile.java
@@ -0,0 +1,50 @@
+package com.rennan.sectorspringboot.domain.service;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+public class Profile {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private long id;
+    private String name;
+    private Date dateAgreeToTerms;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    private List<Sector> sectorList;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDateAgreeToTerms() {
+        return dateAgreeToTerms;
+    }
+
+    public void setDateAgreeToTerms(Date dateAgreeToTerms) {
+        this.dateAgreeToTerms = dateAgreeToTerms;
+    }
+
+    public List<Sector> getSectorList() {
+        return sectorList;
+    }
+
+    public void setSectorList(List<Sector> sectorList) {
+        this.sectorList = sectorList;
+    }
+}
---
+++ src/main/java/com/rennan/sectorspringboot/domain/service/Sector.java
@@ -0,0 +1,40 @@
+package com.rennan.sectorspringboot.domain.service;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Sector {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private long id;
+    private long parentId;
+    private String name;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(long parentId) {
+        this.parentId = parentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
